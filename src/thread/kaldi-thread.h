db9d6d0df983bea9a81038f9c7e5676856fb6097
Frantisek Skala <funous@users.sf.net>
2012-08-17 14:57:52 +0000

    Added missing name to copyright headers.
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/karel@1234 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/thread/kaldi-thread.h
+++ src/thread/kaldi-thread.h
@@ -1,6 +1,7 @@
 // util/kaldi-thread.h
 
 // Copyright 2012  Johns Hopkins University (Author: Daniel Povey)
+//                 Frantisek Skala
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
