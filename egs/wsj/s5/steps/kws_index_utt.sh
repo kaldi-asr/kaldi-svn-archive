f9358977fd2db82b2d60f26fee063c658fc8590a
Lukas Ondel <londel@users.sf.net>
2012-10-01 09:47:11 +0000

    Add recipe for KWS.
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/karel@1415 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ egs/wsj/s5/steps/kws_index_utt.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2012  Lucas Ondel (Brno University of Technology)
+# Apache 2.0
+
+
+# Create a reverted index of all utterances of the lattices provided. The
+# resulting index is a WFST itself.
+
+# Configuration
+acoustic_scale=0.83333
+lm_scale=1.0
+
+echo "$0 $@"  # Print the command line for logging
+
+[ -f path.sh ] && . ./path.sh;
+. parse_options.sh || exit 1;
+
+if [ $# != 4 ]; then
+   echo "Usage: steps/kws_index_utt.sh <lang-dir> <decode-dir> <lattices> <exp-dir>"
+   echo " e.g.: steps/kws_index_utt.sh data/lang  data/train exp/mono0a/decode/lat.* exp/kws/mono0a"
+   echo "main options (for others, see top of script file)"
+   echo "  --acoustic-scale                                 # acoustic scale use for lattice conversion (from Kaldi Lattice to OpenFst WFST)"
+   echo "  --lm-scale                                       # language model scale use for lattice conversion (from Kaldi Lattice to OpenFst WFST)"
+   exit 1;
+fi
+
+lang=$1
+train=$2
+lattices=$3
+dir=$4
+
+mkdir -p $dir
+
+# Create the input/output symbol table
+utils/kws_create_iosyms.awk $lang/words.txt $train/utt2spk > $dir/iosyms.txt
+
+lattice-to-fst --acoustic-scale=$acoustic_scale --lm-scale=$lm_scale ark:$lattices  ark:- |\
+  fstpush-kaldi --remove-total-weight=true --push-in-log=true ark:- ark:- |\
+  fsttoindex $dir/iosyms.txt ark:- ark:- |\
+  fstdeterminizestar --use-log=true ark:- ark:- |\
+  fstpush-kaldi --remove-total-weight=false --push-in-log=true ark:- ark:-|\
+  fstminimize-kaldi --encode ark:- ark:- |\
+  fstunion-kaldi ark:- |\
+  fstrmepsilon |\
+  fstdeterminize-kws > $dir/index.fst
