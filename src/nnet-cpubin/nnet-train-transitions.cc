3d973c552b2d6ccae1ae4fdd2828ca4441e347ce
Dan Povey <dpovey@gmail.com>
2012-10-22 00:19:46 +0000

    Added program to train neural net transitions (and set priors).
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/karel@1482 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/nnet-cpubin/nnet-train-transitions.cc
+++ src/nnet-cpubin/nnet-train-transitions.cc
@@ -22,6 +22,29 @@
 #include "hmm/transition-model.h"
 #include "tree/context-dep.h"
 
+namespace kaldi {
+void SetPriors(const TransitionModel &tmodel,
+               const Vector<double> &transition_accs,
+               double prior_floor,
+               AmNnet *am_nnet) {
+  KALDI_ASSERT(tmodel.NumPdfs() == am_nnet->NumPdfs());
+  Vector<BaseFloat> pdf_counts(tmodel.NumPdfs());
+  KALDI_ASSERT(transition_accs(0) == 0.0); // There is
+  // no zero transition-id.
+  for (int32 tid = 1; tid < transition_accs.Dim(); tid++) {
+    int32 pdf = tmodel.TransitionIdToPdf(tid);
+    pdf_counts(pdf) += transition_accs(tid);
+  }
+  BaseFloat sum = pdf_counts.Sum();
+  KALDI_ASSERT(sum != 0.0);
+  KALDI_ASSERT(prior_floor > 0.0 && prior_floor < 1.0);
+  pdf_counts.Scale(1.0 / sum);
+  pdf_counts.ApplyFloor(prior_floor);
+  pdf_counts.Scale(1.0 / pdf_counts.Sum()); // normalize again.
+  am_nnet->SetPriors(pdf_counts);
+}               
+}
+
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
@@ -35,9 +58,17 @@ int main(int argc, char *argv[]) {
         " nnet-train-transitions 1.nnet \"ark:gunzip -c ali.*.gz|\" 2.nnet\n";
     
     bool binary_write = true;
+    bool set_priors = true; // Also set the per-pdf priors in the model.
+    BaseFloat prior_floor = 1.0e-08;
+    TransitionUpdateConfig transition_update_config;
     
     ParseOptions po(usage);
     po.Register("binary", &binary_write, "Write output in binary mode");
+    po.Register("set-priors", &set_priors, "If true, also set priors in neural "
+                "net (we divide by these in test time)");
+    po.Register("prior-floor", &prior_floor, "When setting priors, floor for "
+                "priors (only used to avoid generating NaNs upon inversion)");
+    transition_update_config.Register(&po);
     
     po.Read(argc, argv);
     
@@ -50,34 +81,56 @@ int main(int argc, char *argv[]) {
         ali_rspecifier = po.GetArg(2),
         nnet_wxfilename = po.GetArg(3);
     
-    ContextDependency ctx_dep;
-    ReadKaldiObject(tree_rxfilename, &ctx_dep);
-    
-    HmmTopology topo;
-    ReadKaldiObject(topo_rxfilename, &topo);
-
-    // Construct the transition model from the tree and the topology file.
-    TransitionModel trans_model(ctx_dep, topo);
-
+    TransitionModel trans_model;
     AmNnet am_nnet;
     {
-      bool binary;
-      Input ki(config_rxfilename, &binary);
-      KALDI_ASSERT(!binary && "Expect config file to contain text.");
-      am_nnet.Init(ki.Stream());
+      bool binary_read;
+      Input ki(nnet_rxfilename, &binary_read);
+      trans_model.Read(ki.Stream(), binary_read);
+      am_nnet.Read(ki.Stream(), binary_read);
+    }
+    
+    Vector<double> transition_accs;
+    trans_model.InitStats(&transition_accs);
+
+    int32 num_done = 0;
+    SequentialInt32VectorReader ali_reader(ali_rspecifier);
+    for (; ! ali_reader.Done(); ali_reader.Next()) {
+      const std::vector<int32> alignment(ali_reader.Value());
+      for (size_t i = 0; i < alignment.size(); i++) {
+        int32 tid = alignment[i];
+        BaseFloat weight = 1.0;
+        trans_model.Accumulate(weight, tid, &transition_accs);
+      }
+      num_done++;
+    }
+    KALDI_LOG << "Accumulated transition stats from " << num_done
+              << " utterances.";
+
+    {
+      BaseFloat objf_impr, count;
+      trans_model.Update(transition_accs, transition_update_config,
+                         &objf_impr, &count);
+      KALDI_LOG << "Transition model update: average " << (objf_impr/count)
+                << " log-like improvement per frame over " << count
+                << " frames.";
     }
 
+    if (set_priors) {
+      KALDI_LOG << "Setting priors of pdfs in the model.";
+      SetPriors(trans_model, transition_accs, prior_floor, &am_nnet);
+    }
+    
     {
       Output ko(nnet_wxfilename, binary_write);
       trans_model.Write(ko.Stream(), binary_write);
       am_nnet.Write(ko.Stream(), binary_write);
     }
-    KALDI_LOG << "Initialized neural net and wrote it to " << nnet_wxfilename;
+    KALDI_LOG << "Trained transitions of neural network model and wrote it to "
+              << nnet_wxfilename;
     return 0;
   } catch(const std::exception &e) {
     std::cerr << e.what() << '\n';
     return -1;
   }
 }
-
-
