5eef5308f5d163077d9f489156893c6b23aff258
Lukas Ondel <londel@users.sf.net>
2012-11-02 13:31:56 +0000

    Add implementation for indexing lattices (General Indexation of Weighted Automata).
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/lucas@1520 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/fstbin/test/kws_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ${PWD##*/} != "test" ];then
+  echo "This script should be run from test directory."
+  exit 1;
+fi
+
+../../latbin/lattice-to-fst --acoustic-scale=1.0 --lm-scale=1.0 ark,t:utts.txt ark,t:- | ../fstpush-kaldi --push-in-log=true ark,t:- ark,t:- | ../fsttoindex isyms.txt ark,t:- ark,t:- | ../fstdeterminizestar --use-log=true ark,t:- ark,t:- |  ../fstpush-kaldi --remove-total-weight=false --push-in-log=true ark,t:- ark,t:- | ../fstminimize-kaldi --encode ark,t:- ark,t:- | ../fstunion-kaldi ark,t:- | ../../../tools/openfst/bin/fstrmepsilon | ../fstdeterminize-kws > test.fst
+
+../../../tools/openfst/bin/fstequal test.fst index.fst
+OUT=$?
+
+if  [ $OUT -eq 0 ]; then
+  echo "Test has succeeded."
+else
+  echo "Error: the newly created index is different from the reference fst."
+fi
+
