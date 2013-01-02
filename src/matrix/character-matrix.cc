ebece577be8b81dfe92eeb926adbdd9dddde5c9a
Pegah Ghahremani <pegahgh@gmail.com>
2013-01-02 21:22:32 +0000

    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/integer@1753 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/matrix/character-matrix.cc
+++ src/matrix/character-matrix.cc
@@ -1,22 +1,5 @@
 #include "character-matrix.h"
-#include <iostream>
-int main() {
-  CharacterMatrix<float> a(2, 2,1);
-  a.SetZero();
-  a.Set(10);
-  a.Resize(4,3,5);
-  std::cout << "The row number is : " << a.NumRows() << " a(0, 0) = " << a(0,0)<< std::endl ;
-  CharacterMatrix<float> b(4,3,0.2);
-  CharacterMatrix<float> c(3,4,0);
-  CharacterMatrix<int> d(4,3,5) ;
-  //std::cout<<" d after change :"<<CharacterMatrix<float>::CopyFromCharacterMatrix2(b)<<std::endl;
-  d = CharacterMatrix<float>::CopyFromCharacterMatrix2(b) ;
-  std::cout<<" d(1,1) is : "<<d(1,1) << std::endl ;
-  c.Transpose(b);
-  float fVal = 60 ;
-  char cVal[32] ;
-  sprintf(cVal, "%f", fVal) ;
-  std::cout<<"cVal :"<< cVal<<std::endl ;
-  //std::cout << "row number : " << c.NumRows() << " c(2, 3) = " << c(2,3)<< std::endl ;
-  return 0;
-}
+ #include <iostream>
+ namespace kaldi {
+ 
+ } // namespace kaldi
