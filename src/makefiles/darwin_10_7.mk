c6ba33db7122ff11eff473fbd6ee1bd447821e8f
Matthew Aylett <matthewaylett@gmail.com>
2013-01-24 17:08:04 +0000

    added idlak make stuff for darwin
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/idlak@1938 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/makefiles/darwin_10_7.mk
+++ src/makefiles/darwin_10_7.mk
@@ -1,4 +1,4 @@
-# makefiles/darwin_10_6.mk contains Darwin-specific rules for OS X 10.6.*
+# makefiles/darwin_10_7.mk contains Darwin-specific rules for OS X 10.7.*
 
 ifndef FSTROOT
 $(error FSTROOT not defined.)
@@ -10,11 +10,18 @@ CXXFLAGS = -msse -msse2 -Wall -I.. \
       -DHAVE_EXECINFO_H=1 -DHAVE_CXXABI_H -rdynamic \
       -DHAVE_CLAPACK \
       -I$(FSTROOT)/include \
+      -I$(PCREROOT)/include \
+      -I$(EXPATROOT)/include \
+      -I$(PUJIXMLROOT)/src \
       $(EXTRA_CXXFLAGS) \
       -g # -O0 -DKALDI_PARANOID
 
 LDFLAGS = -g -rdynamic
-LDLIBS = $(EXTRA_LDLIBS) $(FSTROOT)/lib/libfst.a -ldl -lm -lpthread -framework Accelerate
+ifeq ($(IDLAK),true)
+	LDLIBS =  $(EXTRA_LDLIBS) $(FSTROOT)/lib/libfst.a $(PCREROOT)/lib/libpcrecpp.a $(PCREROOT)/lib/libpcre.a $(PUJIXMLROOT)/scripts/libpugixml.a $(EXPATROOT)/lib/libexpat.a -ldl -lm -lpthread -framework Accelerate
+else
+	LDLIBS =  $(EXTRA_LDLIBS) $(FSTROOT)/lib/libfst.a -ldl -lm -lpthread -framework Accelerate
+endif
 CXX = g++
 CC = g++
 RANLIB = ranlib
