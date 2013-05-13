52e624c896c5241a4001a718565b0cb5e19fb4db
Ehsan Variani <ehsan.variani@gmail.com>
2013-05-13 20:49:48 +0000

    sandbox/dan: cu-tp-matrix.h
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/dan@2438 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/cudamatrix/cu-tp-matrix.h
@@ -0,0 +1,45 @@
+#ifndef KALDI_CUDAMATRIX_CU_TP_MATRIX_H_
+#define KALDI_CUDAMATRIX_CU_TP_MATRIX_H_
+
+#include <sstream>
+
+#include "cudamatrix/cu-common.h"
+#include "matrix/matrix-common.h"
+#include "matrix/tp-matrix.h"
+#include "cudamatrix/cu-stlvector.h"
+#include "cudamatrix/cu-math.h"
+#include "cudamatrix/cu-packed-matrix.h"
+#include "cudamatrix/cu-matrix.h"
+
+namespace kaldi {
+
+template<typename Real> class CuTpMatrix;
+
+template<typename Real>
+class CuTpMatrix : public CuPackedMatrix<Real> {
+ public:
+  CuTpMatrix() : CuPackedMatrix<Real>() {}
+  explicit CuTpMatrix(MatrixIndexT r, MatrixResizeType resize_type = kSetZero)
+      : CuPackedMatrix<Real>(r, resize_type) {}
+  explicit CuTpMatrix<Real>(const TpMatrix<Real> &orig)
+      : CuPackedMatrix<Real>(orig) {}
+  explicit CuTpMatrix<Real>(const CuTpMatrix<Real> &orig)
+      : CuPackedMatrix<Real>(orig) {}
+  
+  ~CuTpMatrix() {}
+
+  inline const TpMatrix<Real> &Mat() const {
+    return *(reinterpret_cast<const TpMatrix<Real>* >(this));
+  }
+
+  inline TpMatrix<Real> &Mat() {
+    return *(reinterpret_cast<TpMatrix<Real>* >(this));
+  }
+  
+  void Cholesky(const CuSpMatrix<Real>& Orig);
+  void Invert();
+};
+
+} // namespace
+
+#endif
