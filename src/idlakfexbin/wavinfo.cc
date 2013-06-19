d7d5064e1b991ca40ca24ab144d00f2843d9196b
Matthew Aylett <matthewaylett@gmail.com>
2013-06-19 11:15:10 +0000

    utility to get samplerate and duration of wav files
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/idlak@2535 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

---
+++ src/idlakfexbin/wavinfo.cc
@@ -0,0 +1,59 @@
+// idlakfexbin/idlakfex.cc
+
+// Copyright 2013 CereProc Ltd.  (Author: Matthew Aylett)
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+// See the Apache 2 License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "feat/wave-reader.h"
+// Simple wrapper around class WaveData to extract sample rate and
+// and duration of a riff wav file
+
+int main(int argc, char *argv[]) {
+  const char *usage =
+      "Get information on a riff wav file\n"
+      "Usage:  wavinfo wavfile\n";
+  std::string wavin;
+  try {
+    kaldi::ParseOptions po(usage);
+    po.Read(argc, argv);
+    // Must have input wavfile
+    if (po.NumArgs() != 1) {
+      po.PrintUsage();
+      exit(1);
+    }
+    wavin = po.GetArg(1);
+    bool binary;
+    kaldi::Input ki(wavin, &binary);
+    kaldi::Output kio("-", false);
+    kaldi::WaveData wav;
+    wav.Read(ki.Stream());
+    kaldi::WriteToken(kio.Stream(), false, "SampleRate:");
+    kaldi::WriteBasicType(kio.Stream(), false, wav.SampFreq());
+    std::endl(kio.Stream());
+    kaldi::WriteToken(kio.Stream(), false, "Duration:");
+    kaldi::WriteBasicType(kio.Stream(), false, wav.Duration());
+    std::endl(kio.Stream());
+    kaldi::WriteToken(kio.Stream(), false, "Samples:");
+    kaldi::WriteBasicType(kio.Stream(), false, wav.Data().NumCols());
+    std::endl(kio.Stream());
+
+    return 0;
+  } catch(const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return -1;
+  }
+}
