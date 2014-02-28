c4bf072665b1fe243a264ebea27aa6ecb2606a8f
Dan Povey <dpovey@gmail.com>
2014-02-28 20:57:48 +0000

    Sandbox/oplatek2: modifications to vystadial recipe to use GridEngine.
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/oplatek2@3634 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/vystadial/s5/path.sh
+++ egs/vystadial/s5/path.sh
@@ -1,10 +1,6 @@
 # Needed for "correct" sorting
 export LC_ALL=C
-
-if [[ -z "$KALDI_ROOT" ]] ; then
-    echo "KALDI_ROOT need to be set"
-    exit 1
-fi
+export KALDI_ROOT=../../..
 
 # adding Kaldi binaries to path
 export PATH=$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/irstlm/bin/:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$PWD:$PATH
