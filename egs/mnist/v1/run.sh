087cc8bc068c4cff1fe0d7b772ef16ab45fa6ec4
Xiaohui Zhang <samuelzhang1104@gmail.com>
2014-05-07 19:41:00 +0000

    sandbox/convnets:added ensemble training recipe for mnist and updated the p-norm recipe
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/convnets@3983 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/mnist/v1/run.sh
+++ egs/mnist/v1/run.sh
@@ -4,6 +4,8 @@
 # This is somewhere to put the data; it can just be a local path if you
 # don't want to give it a permanent home.
 data=/export/corpora5/NIST/MNIST
+export train_cmd="queue.pl -l arch=*64,mem_free=2G,ram_free=2G"  
+dnn_gpu_parallel_opts=(--minibatch-size 512 --max-change 40 --num-jobs-nnet 4 --num-threads 1 --parallel-opts "-l gpu=1" ) 
 
 local/download_data_if_needed.sh $data || exit 1;
 
@@ -25,8 +27,14 @@ dir=exp/nnet2
 steps/nnet2/train_tanh.sh data/train_train data/train_heldout $dir
 steps/nnet2/test.sh data/t10k/ $dir
 
-# train and test using pnorm network. Result: %WER 1.49 [ 149 / 10000, 0 ins, 0 del, 149 sub ]
+# train and test using pnorm network. Result: %WER 1.37 [ 137 / 10000, 0 ins, 0 del, 137 sub ]
 dir=exp/nnet2_pnorm 
-steps/nnet2/train_pnorm.sh --pnorm-input-dim 800 --pnorm-output-dim 400 data/train_train data/train_heldout $dir
+steps/nnet2/train_pnorm.sh --cmd "$train_cmd" --num-hidden-layers 2 --pnorm-input-dim 1200 --pnorm-output-dim 600 --stage -5 \
+ "${dnn_gpu_parallel_opts[@]}" data/train_train data/train_heldout $dir 
 steps/nnet2/test.sh data/t10k/ $dir
 
+# train and test using ensemble trained pnorm network. Result: %WER 1.26 [ 126 / 10000, 0 ins, 0 del, 126 sub ]
+dir=exp/nnet2_ensemble_pnorm
+steps/nnet2/train_pnorm_ensemble.sh --cmd "$train_cmd" --num-hidden-layers 2 --pnorm-input-dim 1200 --pnorm-output-dim 600 --final-beta 3 --ensemble-size 2 \
+ "${dnn_gpu_parallel_opts[@]}" data/train_train data/train_heldout $dir 
+steps/nnet2/test.sh data/t10k/ $dir
