2325b60c92dbbc6885be763b3d754e1186dba981
Dan Povey <dpovey@gmail.com>
2014-05-20 00:24:48 +0000

    trunk: break kws-functions.cc into two parts to fix Windows/Cywgin compilation; cosmetic fices.
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/convnets@4006 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- src/lat/kws-functions.cc
+++ src/lat/kws-functions.cc
@@ -1,4 +1,4 @@
-// lat/kws-functions.cc
+// lat/kws-functions1.cc
 
 // Copyright 2012  Johns Hopkins University (Author: Guoguo Chen)
 
@@ -21,6 +21,9 @@
 #include "lat/kws-functions.h"
 #include "fstext/determinize-star.h"
 #include "fstext/epsilon-property.h"
+
+// We have divided kws-functions.cc into two pieces to overcome "too many
+// sections" and "file too large" problems on Windows.  This is the first part.
 
 namespace kaldi {
 
@@ -204,35 +207,6 @@ class CompactLatticeToKwsProductFstMapper {
                            (arc.weight.Weight() == LatticeWeight::Zero() ?
                             StdXStdprimeWeight::Zero() :
                             StdXStdprimeWeight::One()))),
-                 arc.nextstate);
-  }
-
-  fst::MapFinalAction FinalAction() const { return fst::MAP_NO_SUPERFINAL; }
-
-  fst::MapSymbolsAction InputSymbolsAction() const { return fst::MAP_COPY_SYMBOLS; }
-
-  fst::MapSymbolsAction OutputSymbolsAction() const { return fst::MAP_COPY_SYMBOLS;}
-
-  uint64 Properties(uint64 props) const { return props; }
-};
-
-class KwsProductFstToKwsLexicographicFstMapper {
- public:
-  typedef KwsProductArc FromArc;
-  typedef KwsProductWeight FromWeight;
-  typedef KwsLexicographicArc ToArc;
-  typedef KwsLexicographicWeight ToWeight;
-
-  KwsProductFstToKwsLexicographicFstMapper() {}
-
-  ToArc operator()(const FromArc &arc) const {
-    return ToArc(arc.ilabel, 
-                 arc.olabel, 
-                 (arc.weight == FromWeight::Zero() ?
-                  ToWeight::Zero() :
-                  ToWeight(arc.weight.Value1().Value(), 
-                           StdLStdWeight(arc.weight.Value2().Value1().Value(),
-                                         arc.weight.Value2().Value2().Value()))),
                  arc.nextstate);
   }
 
@@ -448,92 +422,6 @@ void MaybeDoSanityCheck(const KwsLexicographicFst &index_transducer) {
 }
   
 
-void MaybeDoSanityCheck(const KwsProductFst &product_transducer) {
-  typedef KwsProductFst::Arc::Label Label;
-  if (GetVerboseLevel() < 2) return;
-  KwsLexicographicFst index_transducer;
-  Map(product_transducer, &index_transducer, KwsProductFstToKwsLexicographicFstMapper());
-  MaybeDoSanityCheck(index_transducer);
-}
-
-
-// This function replaces a symbol with epsilon wherever it appears
-// (fst must be an acceptor).
-template<class Arc>
-static void ReplaceSymbolWithEpsilon(typename Arc::Label symbol,
-                                     fst::VectorFst<Arc> *fst) {
-  typedef typename Arc::StateId StateId;
-  for (StateId s = 0; s < fst->NumStates(); s++) {
-    for (fst::MutableArcIterator<fst::VectorFst<Arc> > aiter(fst, s);
-         !aiter.Done(); aiter.Next()) {
-      Arc arc = aiter.Value();
-      KALDI_ASSERT(arc.ilabel == arc.olabel);
-      if (arc.ilabel == symbol) {
-        arc.ilabel = 0;
-        arc.olabel = 0;
-        aiter.SetValue(arc);
-      }
-    }
-  }
-}  
-
-
-void DoFactorMerging(KwsProductFst *factor_transducer,
-                     KwsLexicographicFst *index_transducer) {
-  using namespace fst;
-  typedef KwsProductFst::Arc::Label Label;
-
-  // Encode the transducer first
-  EncodeMapper<KwsProductArc> encoder(kEncodeLabels, ENCODE);
-  Encode(factor_transducer, &encoder);
-
-
-  // We want DeterminizeStar to remove epsilon arcs, so turn whatever it encoded
-  // epsilons as, into actual epsilons.
-  {
-    KwsProductArc epsilon_arc(0, 0, KwsProductWeight::One(), 0);
-    Label epsilon_label = encoder(epsilon_arc).ilabel;
-    ReplaceSymbolWithEpsilon(epsilon_label, factor_transducer);
-  }
-    
-
-  MaybeDoSanityCheck(*factor_transducer);
-
-  // Use DeterminizeStar
-  KALDI_VLOG(2) << "DoFactorMerging: determinization...";
-  KwsProductFst dest_transducer;
-  DeterminizeStar(*factor_transducer, &dest_transducer);
-
-  MaybeDoSanityCheck(dest_transducer);
-
-  KALDI_VLOG(2) << "DoFactorMerging: minimization...";
-  Minimize(&dest_transducer);
-
-  MaybeDoSanityCheck(dest_transducer);
-  
-  Decode(&dest_transducer, encoder);
-
-  Map(dest_transducer, index_transducer, KwsProductFstToKwsLexicographicFstMapper());
-}
-
-void DoFactorDisambiguation(KwsLexicographicFst *index_transducer) {
-  using namespace fst;
-  typedef KwsLexicographicArc::StateId StateId;
-
-  StateId ns = index_transducer->NumStates();
-  for (StateId s = 0; s < ns; s++) {
-    for (MutableArcIterator<KwsLexicographicFst> 
-         aiter(index_transducer, s); !aiter.Done(); aiter.Next()) {
-      KwsLexicographicArc arc = aiter.Value();
-      if (index_transducer->Final(arc.nextstate) != KwsLexicographicWeight::Zero())
-        arc.ilabel = s;
-      else
-        arc.olabel = 0;
-      aiter.SetValue(arc);
-    }
-  }
-}
-
 void OptimizeFactorTransducer(KwsLexicographicFst *index_transducer,
                               int32 max_states,
                               bool allow_partial) {
@@ -557,7 +445,5 @@ void OptimizeFactorTransducer(KwsLexicographicFst *index_transducer,
   Minimize(index_transducer);
   Decode(index_transducer, encoder);
 }
-
-
 
 } // end namespace kaldi
