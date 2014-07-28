e9d1361886e2f5c2897797dd35df5de6ef37bbbb
Jan Trmal <jtrmal@gmail.com>
2014-07-28 20:23:14 +0000

    (sandbox/bolt/s5) Bunch of changes I forgot commit last time. The recipe now supports pitch
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/bolt@4219 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/bolt/s5/run.sh
+++ egs/bolt/s5/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Copyright 2012 Chao Weng 
+# Copyright 2012 Chao Weng
+#           2014 Jan Trmal, Hainan Xu, Samuel Zhang
 # Apache 2.0
 
 #exit 1;
@@ -8,40 +9,46 @@
 # one by copying and pasting into the shell.
 # Caution: some of the graph creation steps use quite a bit of memory, so you
 # should run this on a machine that has sufficient memory.
+set -e
+set -o pipefail
 
-. cmd.sh
+. ./path.sh
+. ./cmd.sh
 
+#In the config we expect the path to corpora
+#. ./config.conf
+CALLHOME_MA_CORPUS_A=/export/corpora/LDC/LDC96S34/CALLHOME/ 
+CALLHOME_MA_CORPUS_T=/export/corpora/LDC/LDC96T16/
 
 # Data Preparation, 
-local/hkust_data_prep.sh /mnt/spdb/LDC2005S15
+local/callhome_data_prep.sh   "$CALLHOME_MA_CORPUS_A" "$CALLHOME_MA_CORPUS_T" data/local || exit 1;
+
 # Lexicon Preparation,
-local/hkust_prepare_dict.sh
-
-
-
+local/callhome_prepare_dict.sh || exit 1;
 
 # Phone Sets, questions, L compilation 
 utils/prepare_lang.sh data/local/dict "<UNK>" data/local/lang data/lang
 
 # LM training
-local/hkust_train_lms.sh
+local/callhome_train_lms.sh
 
 # G compilation, check LG composition
-local/hkust_format_data.sh
+local/callhome_format_data.sh
 
 # Now make MFCC features.
 # mfccdir should be some place with a largish disk where you
 # want to store MFCC features.
-mfccdir=mfcc
+mfccdir=param
 for x in train dev; do 
-  steps/make_mfcc.sh --nj 10 data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+  steps/make_mfcc_pitch.sh --nj 32 --cmd "$train_cmd" data/$x exp/make_mfcc/$x $mfccdir || exit 1;
   steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
 done
 # after this, the next command will remove the small number of utterances
 # that couldn't be extracted for some reason (e.g. too short; no such file).
 utils/fix_data_dir.sh data/train || exit 1;
+utils/fix_data_dir.sh data/dev || exit 1;
 
-steps/train_mono.sh --nj 10 \
+steps/train_mono.sh --nj 16 \
   data/train data/lang exp/mono0a || exit 1;
 
 
@@ -52,14 +59,14 @@ utils/mkgraph.sh --mono data/lang_test exp/mono0a exp/mono0a/graph || exit 1
 # exp/mono/decode/
 
 
-
-steps/decode.sh --config conf/decode.config --nj 10 \
-  exp/mono0a/graph data/dev exp/mono0a/decode
+steps/decode.sh --config conf/decode.config --nj 16 \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+  exp/mono0a/graph data/dev exp/mono0a/decode_dev
 
 
 
 # Get alignments from monophone system.
-steps/align_si.sh --nj 10 \
+steps/align_si.sh --nj 16 \
   data/train data/lang exp/mono0a exp/mono_ali || exit 1;
 
 # train tri1 [first triphone pass]
@@ -68,13 +75,15 @@ steps/train_deltas.sh \
 
 # decode tri1
 utils/mkgraph.sh data/lang_test exp/tri1 exp/tri1/graph || exit 1;
-steps/decode.sh --config conf/decode.config --nj 10 \
-  exp/tri1/graph data/dev exp/tri1/decode
+
+steps/decode.sh --config conf/decode.config --nj 16 \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+  exp/tri1/graph data/dev exp/tri1/decode_dev
 
 
 
 # align tri1
-steps/align_si.sh --nj 10 \
+steps/align_si.sh --nj 16 \
   data/train data/lang exp/tri1 exp/tri1_ali || exit 1;
 
 # train tri2 [delta+delta-deltas]
@@ -83,12 +92,13 @@ steps/train_deltas.sh \
 
 # decode tri2
 utils/mkgraph.sh data/lang_test exp/tri2 exp/tri2/graph
-steps/decode.sh --config conf/decode.config --nj 10 \
-  exp/tri2/graph data/dev exp/tri2/decode
+steps/decode.sh --config conf/decode.config --nj 16 \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+  exp/tri2/graph data/dev exp/tri2/decode_dev
 
 # train and decode tri2b [LDA+MLLT]
 
-steps/align_si.sh --nj 10 \
+steps/align_si.sh --nj 16 \
   data/train data/lang exp/tri2 exp/tri2_ali || exit 1;
 
 # Train tri3a, which is LDA+MLLT, 
@@ -96,58 +106,62 @@ steps/train_lda_mllt.sh \
  2500 20000 data/train data/lang exp/tri2_ali exp/tri3a || exit 1;
 
 utils/mkgraph.sh data/lang_test exp/tri3a exp/tri3a/graph || exit 1;
-steps/decode.sh --nj 10 --config conf/decode.config \
-  exp/tri3a/graph data/dev exp/tri3a/decode
+steps/decode.sh --nj 16 --config conf/decode.config \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+  exp/tri3a/graph data/dev exp/tri3a/decode_dev
 # From now, we start building a more serious system (with SAT), and we'll
 # do the alignment with fMLLR.
 
-steps/align_fmllr.sh --nj 10 \
+steps/align_fmllr.sh --nj 16 \
   data/train data/lang exp/tri3a exp/tri3a_ali || exit 1;
 
 steps/train_sat.sh \
   2500 20000 data/train data/lang exp/tri3a_ali exp/tri4a || exit 1;
 
 utils/mkgraph.sh data/lang_test exp/tri4a exp/tri4a/graph
-steps/decode_fmllr.sh --nj 10 --config conf/decode.config \
-  exp/tri4a/graph data/dev exp/tri4a/decode
+steps/decode_fmllr.sh --nj 16 --config conf/decode.config \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+  exp/tri4a/graph data/dev exp/tri4a/decode_dev
 
-steps/align_fmllr.sh --nj 10 \
+steps/align_fmllr.sh --nj 16 \
   data/train data/lang exp/tri4a exp/tri4a_ali
 
 # Building a larger SAT system.
 
 steps/train_sat.sh \
-  3500 100000 data/train data/lang exp/tri4a_ali exp/tri5a || exit 1;
+  3500 160000 data/train data/lang exp/tri4a_ali exp/tri5a || exit 1;
 
 utils/mkgraph.sh data/lang_test exp/tri5a exp/tri5a/graph || exit 1;
-steps/decode_fmllr.sh --nj 10 --config conf/decode.config \
-   exp/tri5a/graph data/dev exp/tri5a/decode || exit 1;
+steps/decode_fmllr.sh --nj 16 --config conf/decode.config \
+  --cmd "$decode_cmd" --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
+   exp/tri5a/graph data/dev exp/tri5a/decode_dev || exit 1;
 
+exit 1
 
-# MMI starting from system in tri5a.  Use the same data (100k_nodup).
+# MMI starting from system in tri5a.  Use the same data (160k_nodup).
 # Later we'll use all of it.
-steps/align_fmllr.sh --nj 10 \
+steps/align_fmllr.sh --nj 16 \
   data/train data/lang exp/tri5a exp/tri5a_ali || exit 1;
-steps/make_denlats.sh --nj 10 --transform-dir exp/tri5a_ali \
+steps/make_denlats.sh --nj 16 --transform-dir exp/tri5a_ali \
   --config conf/decode.config \
   data/train data/lang exp/tri5a exp/tri5a_denlats || exit 1;
 steps/train_mmi.sh --boost 0.1 \
   data/train data/lang exp/tri5a_ali exp/tri5a_denlats exp/tri5a_mmi_b0.1 || exit 1;
-steps/decode.sh --nj 10 --config conf/decode.config \
+steps/decode.sh --nj 16 --config conf/decode.config \
   --transform-dir exp/tri5a/decode \
   exp/tri5a/graph data/dev exp/tri5a_mmi_b0.1/decode || exit 1 ; 
 
 # Do MPE.
 steps/train_mpe.sh data/train data/lang exp/tri5a_ali exp/tri5a_denlats exp/tri5a_mpe || exit 1;
 
-steps/decode.sh --nj 10 --config conf/decode.config \
+steps/decode.sh --nj 16 --config conf/decode.config \
   --transform-dir exp/tri5a/decode \
   exp/tri5a/graph data/dev exp/tri5a_mpe/decode || exit 1 ;
 # Do MCE.
 
 steps/train_mce.sh data/train data/lang exp/tri5a_ali exp/tri5a_denlats exp/tri5a_mce || exit 1;
 
-steps/decode.sh --nj 10 --config conf/decode.config \
+steps/decode.sh --nj 16 --config conf/decode.config \
   --transform-dir exp/tri5a/decode \
   exp/tri5a/graph data/dev exp/tri5a_mce/decode || exit 1 ;
 
