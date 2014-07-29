5b159f2e093a4409e2d041cadb2f795b00ddcc45
Jan Trmal <jtrmal@gmail.com>
2014-07-29 22:19:31 +0000

    (sandbox/bolt/s5) Prevent segmenting english words. Detection of english is done heuristically
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/bolt@4222 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/bolt/s5/local/callhome_mmseg_segment.py
+++ egs/bolt/s5/local/callhome_mmseg_segment.py
@@ -5,6 +5,7 @@ from mmseg import seg_txt
 import re
 
 nonspeech_events=re.compile(r"\<.*\>");
+nonchinese_words=re.compile(r"[A-Z'-]+");
 
 for line in sys.stdin:
   blks = str.split(line)
@@ -12,7 +13,9 @@ for line in sys.stdin:
   for i in range(1, len(blks)):
     if nonspeech_events.match(blks[i]) :
       out_line += " " + blks[i]
-      continue
-    for j in seg_txt(blks[i]):
-      out_line += " " + j
+    elif  nonchinese_words.match(blks[i]):
+      out_line += " " + blks[i]
+    else:
+      for j in seg_txt(blks[i]):
+        out_line += " " + j
   print out_line     
