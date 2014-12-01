37422ed6d7e190864b3c7e213211028640782687
Xiaohui Zhang <samuelzhang1104@gmail.com>
2014-12-01 03:28:54 +0000

    sandbox:bolt modified bolt_mandarin eval pipeline
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/bolt@4664 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/bolt_mandarin/s5/local/prepare_eval_data.sh
+++ egs/bolt_mandarin/s5/local/prepare_eval_data.sh
@@ -5,93 +5,99 @@
 . ./path.sh
 . ./cmd.sh
 . ./conf.sh
+stage=1
+# resegment=false
+# mkdir -p $corpora
+#corpora=./corpora/
+# audio files are in corpora and eval_audio
+# corpora=corpora_eval/LDC2014E57
+corpora=corpora/LDC2014E57/
+eval_audio=/export/corpora5/LDC/LDC2014R65
+# text files are in eval_sets
+# eval_sets=./tmp_xml/
+eval_sets=/export/a09/jtrmal/bolt/CTS/LDC2014R64/
+target=./data/local/
+
+. utils/parse_options.sh
 
 set -e 
 set -o pipefail
 set -x
-stage=0
-corpora=./corpora_eval
-resegment=true
-# mkdir -p $corpora
-target=./data/local/
+
+evaldir=data/local/eval
+[ ! -x $corpora ] && echo "Corpora directory $corpora must exist!" && exit 1;
 
 . utils/parse_options.sh
-if true; then
-  find -L $corpora -ipath "*LDC*" \( -iname "*sph" -o -name "*.flac" \) -iname "MA_*" > $corpora/audio.txt
-  # Test here, needs modification!
-  find -L $corpora -ipath "*LDC*" \( -iname "*scr" -o -name "*.xml" -o -iname "*.txt" \) -iname "MA_*">  $corpora/texts.txt
+mkdir -p $evaldir
+( 
+  find -L $eval_audio  \( -iname "*sph" -o -iname "*.flac" \) -iname "MA_*"
+  find -L $corpora -ipath "*LDC*" \( -iname "*sph" -o -iname "*.flac" \) -iname "MA_*") > $evaldir/audio.txt
+
+if [ $stage -le 0 ]; then
+  for evalset in `find $eval_sets/ -maxdepth 1 -mindepth 1 -type d ` ; do
+    echo $evalset
+    evlname=`basename ${evalset%%_without_transcriptions}`
+    find -L $evalset  \( -iname "*scr" -o -name "*.xml" \) -iname "MA_*"\
+      > $evaldir/${evlname}.list
   
-  local/pair_data_files.pl $corpora/audio.txt $corpora/texts.txt  $corpora
-  # mv $corpora/map.txt $corpora/map.eval.txt
-  # Test here, needs modification!
-  grep -i -F -f conf/list.dev $corpora/map.txt > $corpora/map.eval.txt
+    local/pair_data_files.pl $evaldir/audio.txt $evaldir/${evlname}.list  $evaldir
+    mv $evaldir/map.txt $evaldir/${evlname}.map.txt 
+    
+    cat $evaldir/${evlname}.map.txt | cut -f 1 > $evaldir/${evlname}.audio.list
+    cat $evaldir/${evlname}.map.txt | cut -f 2 > $evaldir/${evlname}.texts.list
+  
+    mkdir -p data/$evlname
+    cat $evaldir/${evlname}.texts.list |\
+    	perl /export/a09/jtrmal/bolt/callhome_azr15_no_gale2/local/bolt_data_convert.pl --empty-trans --numeric-channel-id $evaldir/$evlname.audio.list data/$evlname 
+      	# perl local/bolt_data_convert.pl $evaldir/$evlname.audio.list data/$evlname
+      # text and stm files only contain segmentation infomation. 
+      utils/utt2spk_to_spk2utt.pl < data/$evlname/utt2spk >data/$evlname/spk2utt
+      cp data/$evlname/transcripts.txt data/$evlname/text
+      local/prepare_stm.pl --fragmentMarkers "-" --hesitationToken "<HES>" --oovToken "<UNK>" data/$evlname
+      cat data/$evlname/stm | cut -f1-5 -d' ' | awk '{split($3, a, "-");split(a[2],b,""); c=b[1]; s=b[2];if(s=="")s="0";sub(/A/,"1",c);sub(/B/,"2",c); printf tolower($1)"_"s; printf " "c" "; printf ("%s_%d%d "$4" "$5" <O>\n", tolower(a[1]),s,c);}' > tmp1
+      mv tmp1 data/$evlname/stm
+      cp conf/glm data/$evlname
+      utils/fix_data_dir.sh data/$evlname
+  done
 fi
 
-mkdir -p $target
-for dataset in eval; do
-  if [ $stage -le 0 ]; then
-    output=$target/$dataset.xml
-    mkdir -p $output
-    echo $output
-    cat $corpora/map.${dataset}.txt | grep -F ".su.xml" | cut -f 1 > $output/audio.list
-    cat $corpora/map.${dataset}.txt | grep -F ".su.xml" | cut -f 2 > $output/texts.list
-    cat $output/texts.list |\
-    	perl local/bolt_data_convert.pl $output/audio.list $output/
-    output=$target/$dataset.scr
-    mkdir -p $output
-    echo $output
-    (set +o pipefail; cat $corpora/map.${dataset}.txt | grep -v -F ".su.xml" | cut -f 1 )> $output/audio.list
-    (set +o pipefail; cat $corpora/map.${dataset}.txt | grep -v -F ".su.xml" | cut -f 2 )> $output/texts.list
-  
-    cat $output/texts.list |\
-    	perl local/callhome_data_convert.pl $output/audio.list $output/
-  
-    utils/combine_data.sh --extra-files "transcripts.txt" $target/$dataset $target/$dataset.scr $target/$dataset.xml 
-    # text and stm files only contain segmentation infomation. 
-    cp $target/$dataset/transcripts.txt $target/$dataset/text
-    local/prepare_stm.pl --fragmentMarkers "-" --hesitationToken "<HES>" --oovToken "<UNK>" $target/$dataset
-  
-    cat $target/$dataset/stm | cut -f1-5 -d' ' | awk '{split($3, a, "-");split(a[2],b,""); c=b[1]; s=b[2];if(s=="")s="0";sub(/A/,"1",c);sub(/B/,"2",c); printf tolower($1)"_"s; printf " "c" "; printf ("%s_%d%d "$4" "$5" <O>\n", tolower(a[1]),s,c);}' > tmp1
-    mv tmp1 $target/$dataset/stm
-
-    cp conf/glm data/local/${dataset}
-    cp -R data/local/eval data/eval
-  fi
-
-  if [ $stage -le 1 ]; then
+if [ $stage -le 1 ]; then
+  for dataset in PROGRESS VALIDATION; do
     echo "Extracting mfcc features for ${dataset} data"
     mfccdir=param
-    steps/make_mfcc_pitch.sh --nj 32 --cmd "$train_cmd" data/$dataset exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
+    steps/make_mfcc_pitch.sh --nj 64 --cmd "$train_cmd" data/$dataset exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
+    utils/fix_data_dir.sh data/$dataset
     steps/compute_cmvn_stats.sh data/${dataset} exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
-  fi
+    utils/fix_data_dir.sh data/$dataset
+    utils/validate_data_dir.sh --no-text  data/$dataset
+  done
+fi
 
-  # for segments longer than thr (in seconds), re-segment them using automatic segmentations.
-  if $resegment && [ $stage -le 2 ]; then
-    thr=10
-    echo "Doing resegmentation for ${dataset} data. Threshold is $thr"
-    cp -r data/${dataset} data/${dataset}.long
-    cat data/${dataset}/segments | awk -v thr=$thr '{if($4-$3 > thr)print $0}' > data/${dataset}.long/segments
-    utils/fix_data_dir.sh data/${dataset}.long
-    
-    mkdir -p data/${dataset}.short
-    cp data/${dataset}/{reco2file_and_channel,segments,spk2utt,utt2spk,wav.scp} data/${dataset}.short
-    cat data/${dataset}/segments | awk -v thr=$thr '{if($4-$3 <= thr)print $0}' > data/${dataset}.short/segments
-    utils/fix_data_dir.sh data/${dataset}.short
-    
-    # steps/make_phone_graph.sh data/lang exp/tri3a exp/tri3a
-    rm -rf exp/tri3a/decode_${dataset}.long tmp
-    ./local/resegment/generate_subsegments.sh --nj 1 data/${dataset}.long exp/tri3a tmp data/${dataset}.long.seg
-    mv data/${dataset} data/${dataset}.bk
-    utils/combine_data.sh data/${dataset} data/${dataset}.short data/${dataset}.long.seg
-  fi
+#   # for segments longer than thr (in seconds), re-segment them using automatic segmentations.
+#   if $resegment && [ $stage -le 2 ]; then
+#     thr=10
+#     echo "Doing resegmentation for ${dataset} data. Threshold is $thr"
+#     cp -r data/${dataset} data/${dataset}.long
+#     cat data/${dataset}/segments | awk -v thr=$thr '{if($4-$3 > thr)print $0}' > data/${dataset}.long/segments
+#     utils/fix_data_dir.sh data/${dataset}.long
+#     
+#     mkdir -p data/${dataset}.short
+#     cp data/${dataset}/{reco2file_and_channel,segments,spk2utt,utt2spk,wav.scp} data/${dataset}.short
+#     cat data/${dataset}/segments | awk -v thr=$thr '{if($4-$3 <= thr)print $0}' > data/${dataset}.short/segments
+#     utils/fix_data_dir.sh data/${dataset}.short
+#     
+#     # steps/make_phone_graph.sh data/lang exp/tri3a exp/tri3a
+#     rm -rf exp/tri3a/decode_${dataset}.long tmp
+#     ./local/resegment/generate_subsegments.sh --nj 1 data/${dataset}.long exp/tri3a tmp data/${dataset}.long.seg
+#     mv data/${dataset} data/${dataset}.bk
+#     utils/combine_data.sh data/${dataset} data/${dataset}.short data/${dataset}.long.seg
+#   fi
+# 
+#   # re-compute mfcc for re-segmented audio files
+#   if $resegment && [ $stage -le 3 ]; then
+#     echo "Extracting mfcc features for fixed ${dataset} data"
+#     mfccdir=param
+#     steps/make_mfcc_pitch.sh --nj 32 --cmd "$train_cmd" data/$dataset exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
+#     steps/compute_cmvn_stats.sh data/${dataset} exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
+#   fi
 
-  # re-compute mfcc for re-segmented audio files
-  if $resegment && [ $stage -le 3 ]; then
-    echo "Extracting mfcc features for fixed ${dataset} data"
-    mfccdir=param
-    steps/make_mfcc_pitch.sh --nj 32 --cmd "$train_cmd" data/$dataset exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
-    steps/compute_cmvn_stats.sh data/${dataset} exp/make_mfcc/${dataset}.original $mfccdir || exit 1;
-  fi
-done
-
-# after this, the next command will remove the small number of utterances
--- egs/bolt_mandarin/s5/local/run_eval.sh
+++ egs/bolt_mandarin/s5/local/run_eval.sh
@@ -8,13 +8,13 @@ set -o pipefail
 . ./path.sh
 . ./conf.sh
 
-dataset_id=eval
+dataset_id=PROGRESS
 data=data/$dataset_id
-decode_nj=12
+decode_nj=64
 graph=exp/tri5a/graph
 decode_extra_opts=(--num-threads 6 --parallel-opts "-pe smp 6 -l mem_free=4G,ram_free=0.7G")
 opt_mpe_epoch=4
-opt_lmwt=13
+opt_lmwt=15
 SortingProgram=`which hubscr.pl` || SortingProgram=$KALDI_ROOT/tools/sctk/bin/hubscr.pl
 
 ./local/prepare_eval_data.sh
@@ -27,15 +27,13 @@ if [ -f exp/tri5a/.done ]; then
        --skip-scoring true \
        --config conf/decode.config  --scoring_opts "--min_lmwt 8 --max_lmwt 14 "\
        $graph $data $decode || exit 1;
-    local/lattice_to_ctm.sh --min_lmwt 8 --max_lmwt 14 --cmd "$train_cmd" $data $graph $decode
+    local/lattice_to_ctm.sh --min_lmwt 8 --max_lmwt 18 --cmd "$train_cmd" $data $graph $decode
     touch $decode/.done
   fi
   # post-processing of the optimal ctm
   opt_ctm=${decode}/score_${opt_lmwt}/${dataset_id}.ctm
   $SortingProgram sortCTM <$opt_ctm  >${opt_ctm}.sorted 
-  paste -d ' ' <(cut -f 1-4 -d ' ' ${opt_ctm}.sorted ) \
-      <(cut -f 5-  -d ' ' ${opt_ctm}.sorted | uconv -f utf8 -t utf8 -x Any-Lower ) > ${opt_ctm} 
-  cp ${opt_ctm} ${dataset_id}.sat.ctm 
+  cp ${opt_ctm}.sorted ${dataset_id}.sat.ctm 
 fi
 
 # DNN mpe decode
@@ -51,7 +49,7 @@ if [ -f exp/tri6_nnet_mpe/.done ]; then
         "${decode_extra_opts[@]}" \
         --transform-dir exp/tri5a/decode_${dataset_id} \
         $graph $data $decode | tee $decode/decode.log
-      local/lattice_to_ctm.sh --min_lmwt 8 --max_lmwt 14 --cmd "$train_cmd" $data $graph $decode
+      local/lattice_to_ctm.sh --min_lmwt 8 --max_lmwt 18 --cmd "$train_cmd" $data $graph $decode
       touch $decode/.done
     fi
       # rescore with large LM
@@ -60,15 +58,13 @@ if [ -f exp/tri6_nnet_mpe/.done ]; then
           --skip-scoring true \
           --cmd "$decode_cmd" data/lang_test data/lang_test_large \
           $data $decode ${decode}_large
-      local/lattice_to_ctm.sh --min_lmwt 8 --max_lmwt 14 --cmd "$train_cmd" $data $graph ${decode}_large
       touch ${decode}_large/.done
     fi   
+    local/lattice_to_ctm.sh --min_lmwt 14 --max_lmwt 18 --cmd "$train_cmd" $data $graph ${decode}_large
     # post-processing of the optimal ctm
     opt_ctm=${decode}_large/score_${opt_lmwt}/${dataset_id}.ctm
     $SortingProgram sortCTM <$opt_ctm  >${opt_ctm}.sorted 
-    paste -d ' ' <(cut -f 1-4 -d ' ' ${opt_ctm}.sorted ) \
-        <(cut -f 5-  -d ' ' ${opt_ctm}.sorted | uconv -f utf8 -t utf8 -x Any-Lower ) > ${opt_ctm} 
-    cp ${opt_ctm} ${dataset_id}.dnn_mpe.ctm 
+    cp ${opt_ctm}.sorted ${dataset_id}.dnn_mpe.ctm 
   done     
 fi        
           
