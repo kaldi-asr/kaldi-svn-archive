97e3148f26739954cebf3623ff6c59fe7a65aab7
Jan Trmal <jtrmal@gmail.com>
2014-12-03 21:51:59 +0000

    (sandbox/bolt/arz) Adding last minute changes to the high level scripts done last week
    git-svn-id: file:///home/jtrmal/Downloads/kaldi-git-conversion/kaldi-svn/sandbox/bolt@4667 5e6a8d80-dfce-4ca6-a32a-6e07a63d50c8

--- egs/callhome_egyptian/s5/runx_mpe.sh
+++ egs/callhome_egyptian/s5/runx_mpe.sh
@@ -35,47 +35,29 @@ steps/nnet2/align.sh --use-gpu no  --cmd "$decode_cmd" \
   --transform-dir exp/tri5b_ali --nj $train_nj \
   data/train data/lang exp/tri6b_dnn exp/tri6b_dnn_ali || exit 1
 
-
-steps/nnet2/train_discriminative.sh \
+steps/nnet2/train_discriminative.sh --stage 0\
   --cmd "$decode_cmd" \
   --learning-rate $dnn_mpe_learning_rate \
   --modify-learning-rates true \
   --last-layer-factor $dnn_mpe_last_layer_factor \
-  --num-epochs 4 --cleanup true \
+  --num-epochs 16 --cleanup true \
   --retroactive true \
+  --cleanup false \
   --transform-dir exp/tri5b_ali \
   "${dnn_gpu_mpe_parallel_opts[@]}" data/train data/lang \
   exp/tri6b_dnn_ali exp/tri6b_dnn_denlats exp/tri6b_dnn/final.mdl exp/tri6b_dnn_mpe || exit 1
 
 
-for epoch in 1 2 3 4; do
+for epoch in `seq 1 8`; do
   decode=exp/tri6b_dnn_mpe/decode_tune_epoch$epoch
   mkdir -p $decode
   steps/nnet2/decode.sh --minimize true \
     --cmd "$decode_cmd" --nj 13 --iter epoch$epoch \
     --beam 16 --lattice-beam 8.5 \
     --transform-dir exp/tri5b/decode_tune \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
+    --scoring-opts "--min-lmwt 8 --max-lmwt 16" \
+    --num-threads 6 --parallel-opts "-pe smp 6" \
     exp/tri5b/graph data/tune $decode | tee $decode/decode.log
 done
 
-
-steps/nnet2/train_discriminative.sh \
-  --cmd "$decode_cmd" \
-  --num-epochs 4 --cleanup true \
-  --transform-dir exp/tri5b_ali \
-  "${dnn_gpu_mpe_parallel_opts[@]}" data/train data/lang \
-  exp/tri6b_dnn_ali exp/tri6b_dnn_denlats exp/tri6b_dnn/final.mdl exp/tri6b_dnn_mpe_2 || exit 1
-
-
-for epoch in 1 2 3 4; do
-  decode=exp/tri6b_dnn_mpe_2/decode_tune_epoch$epoch
-  mkdir -p $decode
-  steps/nnet2/decode.sh --minimize true \
-    --cmd "$decode_cmd" --nj 13 --iter epoch$epoch \
-    --beam 16 --lattice-beam 8.5 \
-    --transform-dir exp/tri5b/decode_tune \
-    --num-threads 4 --parallel-opts "-pe smp 4" \
-    exp/tri5b/graph data/tune $decode | tee $decode/decode.log
-done
 
